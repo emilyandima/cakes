9dec5841f9e5b52af03b2f3f18ee60549f0e8b70
nyamweya <nyamweyadaniel@gmail.com>
2019-11-14 14:01:48 +0300

    Added cakes project

---
+++ wp-content/themes/blossom-recipe/inc/getting-started/tabs/link-panel.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Right Buttons Panel.
+ *
+ * @package Blossom_Recipe
+ */
+?>
+<div class="panel-right">
+	<div class="panel-aside">
+		<h4><?php esc_html_e( 'Upgrade To Pro', 'blossom-recipe' ); ?></h4>
+		<p><?php esc_html_e( 'The premium theme provides you better customization option to change the look and feel of your website. You can change the primary color, background color, use image or pattern in the background, and even customize the fonts with the premium theme. Furthermore, you will get multiple header layouts, homepage layouts, blog post layouts, and advanced control over the slider or banner. You can also make a multi-language website with the premium version as they are fully compatible with Polylang and WPML plugin.', 'blossom-recipe' ); ?></p>
+		<p><?php esc_html_e( 'The premium themes are backed up by the priority customer support team and gets quicker update.', 'blossom-recipe' ); ?></p>
+		<a class="button button-primary" href="<?php echo esc_url( 'https://blossomthemes.com/downloads/blossom-recipe-pro/' ); ?>" title="<?php esc_attr_e( 'View Premium Version', 'blossom-recipe' ); ?>" target="_blank">
+            <?php esc_html_e( 'Read More About the Pro Theme', 'blossom-recipe' ); ?>
+        </a>
+	</div><!-- .panel-aside Theme Support -->
+	<!-- Knowledge base -->
+	<div class="panel-aside">
+		<h4><?php esc_html_e( 'Visit the Knowledge Base', 'blossom-recipe' ); ?></h4>
+		<p><?php esc_html_e( 'Need help with using the WordPress as quickly as possible? Visit our well-organized Knowledge Base!', 'blossom-recipe' ); ?></p>
+		<p><?php esc_html_e( 'Our Knowledge Base has step-by-step video and text tutorials, from installing the WordPress to working with themes and more.', 'blossom-recipe' ); ?></p>
+
+		<a class="button button-primary" href="https://docs.blossomthemes.com/docs/blossom-recipe/" title="<?php esc_attr_e( 'Visit the knowledge base', 'blossom-recipe' ); ?>" target="_blank"><?php esc_html_e( 'Visit the Knowledge Base', 'blossom-recipe' ); ?></a>
+	</div><!-- .panel-aside knowledge base -->
+
+	<div class="panel-aside">
+		<h4><?php _e( 'Submit your site for social share', 'blossom-recipe' ); ?></h4>
+		<p><?php _e( 'We regularly share and feature websites made using our themes on our social media accounts( Facebook, Instagram, Twitter and Pinterest ).', 'blossom-recipe' ); ?></p>
+		<p><?php _e( 'If you would like to get your website shared and featured, please submit your website by clicking the link below.', 'blossom-recipe' ); ?></p>
+
+		<a class="button button-primary" href="<?php echo esc_url( 'https://blossomthemes.com/submit-your-site-for-social-share/' ); ?>" title="<?php esc_attr_e( 'Submit your site for social share', 'blossom-recipe' ); ?>" target="_blank"><?php _e( 'Submit Here', 'blossom-recipe' ); ?></a>
+	</div><!-- .panel-aside knowledge base -->
+</div><!-- .panel-right -->
